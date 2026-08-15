71716ae34297f8de34a21f9e07d9df29f26b81d5
dharshanaangamuthu4088-ship-it <dharshanaangamuthu4088@gmail.com>
2026-08-15 17:36:00 +0530

    Add Goal Parser Interpretation solution

---
+++ strings/Goal Parser Interpretation(1678).py
@@ -0,0 +1,16 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        s=""
+        i=0
+        while i<len(command):
+            if command[i]=="(":
+                if command[i+1]==")":
+                   s+="o"
+                   i+=2 
+                elif command[i+1]=="a":
+                    s+="al"
+                    i+=4
+            else:
+                s+="G"
+                i+=1
+        return s
